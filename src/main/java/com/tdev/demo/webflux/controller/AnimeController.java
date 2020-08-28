834e8e7a250580e0827c842000279a4cf1e17278
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-08-27 23:25:25 -0300

    crating anime controller

---
+++ src/main/java/com/tdev/demo/webflux/controller/AnimeController.java
@@ -0,0 +1,23 @@
+package com.tdev.demo.webflux.controller;
+
+import com.tdev.demo.webflux.domain.Anime;
+import com.tdev.demo.webflux.repository.AnimeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("animes")
+@Slf4j
+public class AnimeController {
+
+    private final AnimeRepository animeRepository;
+
+    public Flux<Anime> listAll() {
+        return animeRepository.findAll();
+    }
+
+}
