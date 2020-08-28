bc8046c540bf54406537688ffbeafa7c3fd7e615
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-08-27 23:17:22 -0300

    initial commit

---
+++ src/test/java/com/tdev/demo/webflux/SpringWebfluxEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdev.demo.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
