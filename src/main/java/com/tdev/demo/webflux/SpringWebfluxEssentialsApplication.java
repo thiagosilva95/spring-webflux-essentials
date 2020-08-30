ddff66190d58b28866b8ddbe3b9cc02a4f9ba85e
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-08-30 11:04:02 -0300

    switch if empty

--- src/main/java/com/tdev/demo/webflux/SpringWebfluxEssentialsApplication.java
+++ src/main/java/com/tdev/demo/webflux/SpringWebfluxEssentialsApplication.java
@@ -2,14 +2,13 @@ package com.tdev.demo.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringWebfluxEssentialsApplication {
 
-	static {
+	/*static {
 		BlockHound.install();
-	}
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxEssentialsApplication.class, args);
--- src/main/java/com/tdev/demo/webflux/service/AnimeService.java
+++ src/main/java/com/tdev/demo/webflux/service/AnimeService.java
@@ -3,7 +3,9 @@ package com.tdev.demo.webflux.service;
 import com.tdev.demo.webflux.domain.Anime;
 import com.tdev.demo.webflux.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -18,6 +20,11 @@ public class AnimeService {
     }
 
     public Mono<Anime> findById(int id) {
-        return animeRepository.findById(id);
+        return animeRepository.findById(id)
+                .switchIfEmpty(monoResponseStatusNotFoundException());
+    }
+
+    public <T> Mono<T> monoResponseStatusNotFoundException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
     }
 }
